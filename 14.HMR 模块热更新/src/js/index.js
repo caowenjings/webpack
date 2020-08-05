4bb84eb110e30d71e9770e9ec79fde9a7180d490
withease-partner6 <withease-partner6@hashdash-group.com>
2020-08-05 16:21:02 +0800

    添加了模块热更新

--- 14.HMR 模块热更新/src/js/index.js
+++ 14.HMR 模块热更新/src/js/index.js
@@ -1,8 +1,67 @@
 import "../css/index.less";
-
+import { start } from './timer'
+import { message } from './foo'
 console.log("加载了js")
-function add(x, y) {
-    return x + y;
+
+// 1.没有热更新时
+//改变timer.js的延迟时间时，计数会从0开始计数
+// start(Update, 0);
+
+
+
+/* 
+module.hot.accept() 告诉 Webpack，当前模块更新不用刷新
+module.hot.decline() 告诉 Webpack，当前模块更新时一定要刷新
+在热更新的机制中，以这种“声明”的方式告知 Webpack，
+哪些模块的更新是被处理的，
+哪些模块的更新又不被处理
+*/
+
+// 2.有热更新时
+//改变timer.js的延迟时间时，计数会接着开始
+
+var current = 0
+
+if (module.hot && module.hot.data) {
+    current = module.hot.data.current
+}
+var root = document.getElementById('root')
+var stop = start(onUpdate, current)
+
+
+if (module.hot) {
+
+    module.hot.accept()
+    module.hot.dispose(data => {
+        data.current = current
+        stop()
+    })
+
+
+    module.hot.accept('./timer', function () {
+        stop()
+        stop = start(onUpdate, current)
+    })
+    module.hot.decline('./foo')
+}
+function onUpdate(i) {
+    current = i
+    root.textContent = '#' + i // 修改数值渲染
 }
 
-console.log(add(1, 3));
+/*
+3.处理自身模块
+module.hot.dispose() 用于注册当前模块被替换前的处理函数，并且回调函数接收一个 data 对象，可以向其写入需要保存的数据，这样在新的模块执行时可以通过 module.hot.data 获取到：
+首先，模块执行时，先检查有没有旧模块留下来的数据，如果有，就恢复。
+然后在模块被替换前的执行处理，这里就是记录数据、停掉现有的定时器：
+做了这些处理之后，修改 index.js 的 onUpdate，使得渲染到页面的数值改变，也可以在不刷新的情况下体现：
+
+if (module.hot && module.hot.data) {
+    current = module.hot.data.current
+}
+module.hot.accept()
+module.hot.dispose(data => {
+    data.current = current
+    stop()
+})
+*/
--- 14.HMR 模块热更新/webpack.config.js
+++ 14.HMR 模块热更新/webpack.config.js
@@ -1,24 +1,33 @@
 /*
 一般情况下，一个模块发生变化，比如只改变一个css文件，js文件没有变化，结果全部重新加载 
 
+
 HMR：hot module replacement 热模块替换/模块热替换
 作用：一个模块发生变化，只会重新打包这一个模块（而不是打包所有模块）
      极大提升构建速度
 
      样式文件：可以使用HRM功能，style-loader内部实现了
      js文件： js文件默认不能使用HRM功能
-             解决：修改js代码，添加支持HRM的代码
-             注意：模块热更新，不能用在入口的主文件里，只能用在分支上面
+              解决：修改js代码，添加支持HRM的代码
+              注意：模块热更新，不能用在入口的主文件里，只能用在分支上面
      html文件： html文件默认不能使用HRM功能，同时还会导致问题：html文件不能热更新了，改变了东西不能在页面上改变了
                解决：修改entry入口，将html文件引入，依旧默认不能使用HRM功能
                (只有一个html文件，不需要做HRM功能)
+
+
+启动：1.HotModuleReplacementPlugin 插件是 Webpack 自带的，在 webpack.config.js 加入就好：
+       plugins: [ webpack.HotModuleReplacementPlugin(),] 
+     
+     2.直接通过 webpack-dev-server 启动 Webpack 的开发环境：
+     devServer: {hot: true,}
 */
 
 
 const { resolve } = require("path");
+const webpack = require('webpack');
 const HtmlWebpackPlugins = require("html-webpack-plugin");
 module.exports = {
-    entry: ["./src/js/index.js"],
+    entry: ["./src/js/index.js", "./src/index.html"],
     output: {
         filename: "js/built.js",//输出到js文件夹
         path: resolve(__dirname, "built")
@@ -60,7 +69,9 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugins({
             template: './src/index.html'
-        })
+        }),
+
+        new webpack.HotModuleReplacementPlugin()
     ],
     mode: "development",
     // 启动devServer的命令：npx webpack-dev-server
